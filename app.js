3ecf20f25a2c1cdb90c5fae2c7ee5e688639f6e9
NaveenChahar <40751814+NaveenChahar@users.noreply.github.com>
2018-12-31 14:20:54 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,24 +1,8 @@
 const express=require("express");
 const cors=require("cors");
 const app=express();
-const multer = require('multer');
-const path  = require('path');
 
 console.log('req is here');
-
-const upload = require('./api/multer/uploadCode');
-
-const storage = multer.diskStorage({
-    destination:'./uploads/',
-    filename:function(req,file,cb){
-        cb(null,file.fieldname + path.extname(file.originalname));
-
-    }
-})
-
-const uploadDetails = upload(storage);
-
-
 
 const port=process.env.port||1234;
 app.use(cors());
@@ -29,6 +13,7 @@ app.use(express.static("public"));
 const bodyParser=require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
 
 
 var routes = require('./api/routes/empRoutes'); //importing route
