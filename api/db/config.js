693cea6c5e4c3f6300bd121035d9e5380cfd9a20
anmolrajaroraa <anmolarora1711@gmail.com>
2019-01-04 14:03:23 +0530

    Update config.js

--- api/db/config.js
+++ api/db/config.js
@@ -1,5 +1,5 @@
 const db={
-    url : "mongodb://<dbuser>:<dbpassword>@ec2-13-232-101-237.ap-south-1.compute.amazonaws.com:27017/dummyDB"
+    url : "mongodb://big-basket:RYrVME4Gna2S@ec2-13-232-101-237.ap-south-1.compute.amazonaws.com:27017/dummyDB"
 }
 
 module.exports=db;
